10978f17484adc4c265058b4ec5ec5d6abb39640
Briana Dacoscos <briana.dacoscos@gmail.com>
2018-05-22 20:58:22 -0700

    add some templating

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,8 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  console.log('Hey! You!');
-  res.send('Hey! It works!');
+  const bri = { name: 'Bri', age: 80, cool: true};
+  // console.log('Hey! You!');
+  // res.send('Hey! It works!');
+  // res.json(bri);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'Briana',
+    cat: req.query.cat
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
