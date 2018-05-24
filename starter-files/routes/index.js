0907463f4350491a52bc58b2996d2722c7b9bf1d
Briana Dacoscos <briana.dacoscos@gmail.com>
2018-05-23 20:49:32 -0700

    add new controller and index page

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+
 
 // Do work here
-router.get('/', (req, res) => {
-  const bri = { name: 'Bri', age: 80, cool: true};
-  // console.log('Hey! You!');
-  // res.send('Hey! It works!');
-  // res.json(bri);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'Briana',
-    cat: req.query.cat
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
